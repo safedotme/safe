e6e83a8053982449209f715d92715a604f3f8a4c
Mark Music <markmusic999@gmail.com>
2022-07-02 23:43:18 -0600

    ft[ios && android && main.dart] => 🐋 Setup Firebase project with .env key privacy

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,41 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'package:safe/core.dart';
+import 'package:safe/utils/firebase.util.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  await Firebase.initializeApp(
+    options: FirebaseUtil().currentPlatform,
+  );
+
+  runApp(Safe());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
+class Safe extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        body: Center(
-          child: Text("Hello world"),
+    return MultiProvider(
+      providers: [
+        Provider<Core>(create: (_) => Core()),
+      ],
+      child: MaterialApp(
+        title: "Safe",
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Center(
+            child: Text(
+              "Hello world",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 30,
+                fontFamily: "RoundedHeavy",
+              ),
+            ),
+          ),
         ),
       ),
     );
