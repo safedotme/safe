65e5012f160348c79261c08b488e07ed144882bd
Mark Music <markmusic999@gmail.com>
2022-07-03 00:57:47 -0600

    ft[constants] => ☄️ Add primary colors

--- lib/utils/constants.util.dart
+++ lib/utils/constants.util.dart
@@ -34,6 +34,7 @@ enum Transparency {
 }
 
 const Map<MutableColor, Color> kColorMap = {
+  // NEUTRALS
   MutableColor.neutral1: Colors.white,
   MutableColor.neutral2: Color(0xffB4B5B9),
   MutableColor.neutral3: Color(0xff656565),
@@ -44,4 +45,11 @@ const Map<MutableColor, Color> kColorMap = {
   MutableColor.neutral8: Color(0xff151317),
   MutableColor.neutral9: Color(0xff0F0D10),
   MutableColor.neutral10: Color(0xff070508),
+
+  //PRIMARIES
+  MutableColor.primaryYellow: Color(0xffF8D849),
+  MutableColor.primaryOrange: Color(0xffEE8131),
+  MutableColor.primaryRed: Color(0xffEA336C),
+  MutableColor.primaryMagenta: Color(0xffC127BF),
+  MutableColor.primaryPurple: Color(0xff6E3CF1),
 };
