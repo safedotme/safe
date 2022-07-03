fc15421a7d5e2bb159abb2d30f777ce406731666
Mark Music <markmusic999@gmail.com>
2022-07-03 01:02:08 -0600

    ft[constants] => 🌏 Add all colors to constants in map format

--- lib/utils/constants.util.dart
+++ lib/utils/constants.util.dart
@@ -21,20 +21,8 @@ enum MutableColor {
   secondaryRed,
 }
 
-enum Transparency {
-  opaque,
-  v80,
-  v64,
-  v56,
-  v40,
-  v24,
-  v16,
-  v8,
-  v4,
-}
-
 const Map<MutableColor, Color> kColorMap = {
-  // NEUTRALS
+  // NEUTRAL
   MutableColor.neutral1: Colors.white,
   MutableColor.neutral2: Color(0xffB4B5B9),
   MutableColor.neutral3: Color(0xff656565),
@@ -46,10 +34,26 @@ const Map<MutableColor, Color> kColorMap = {
   MutableColor.neutral9: Color(0xff0F0D10),
   MutableColor.neutral10: Color(0xff070508),
 
-  //PRIMARIES
+  //PRIMARY
   MutableColor.primaryYellow: Color(0xffF8D849),
   MutableColor.primaryOrange: Color(0xffEE8131),
   MutableColor.primaryRed: Color(0xffEA336C),
   MutableColor.primaryMagenta: Color(0xffC127BF),
   MutableColor.primaryPurple: Color(0xff6E3CF1),
+
+  // SECONDARY
+  MutableColor.secondaryGreen: Color(0xff50C166),
+  MutableColor.secondaryRed: Color(0xffFC645D),
 };
+
+enum Transparency {
+  opaque,
+  v80,
+  v64,
+  v56,
+  v40,
+  v24,
+  v16,
+  v8,
+  v4,
+}
