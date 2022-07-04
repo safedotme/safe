0496c8d59d3e9c1cecc45a51d844ec32002a053b
Mark Music <markmusic999@gmail.com>
2022-07-04 17:44:59 -0600

    ft[mutableBanner] => 👓 Build MutableBanner widget without opacity implementation

--- lib/screens/welcome/welcome.screen.dart
+++ lib/screens/welcome/welcome.screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:safe/core.dart';
 import 'package:safe/widgets/mutable_banner/mutable_banner.widget.dart';
 import 'package:safe/widgets/mutable_scaffold/mutable_scaffold.widget.dart';
+import 'package:safe/widgets/mutable_text/mutable_text.widget.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = "welcome_screen";
@@ -13,24 +14,39 @@ class WelcomeScreen extends StatefulWidget {
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
   late Core core;
+  late BannerController controller;
 
   @override
   void initState() {
     super.initState();
 
     core = Provider.of<Core>(context, listen: false);
+    controller = BannerController();
   }
 
   @override
   Widget build(BuildContext context) {
     return MutableScaffold(
-      overlays: [
-        MutableBanner(
-          title: "This is the title",
-          description: "This is the description",
-        ),
-      ],
-      body: Container(),
-    );
+        overlays: [
+          MutableBanner(
+            title: "This is the title",
+            description: "This is the description",
+            controller: controller,
+          ),
+        ],
+        body: SizedBox.expand(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  controller.show();
+                },
+                child: MutableText("Show Banner"),
+              ),
+            ],
+          ),
+        ));
   }
 }
--- lib/widgets/mutable_banner/mutable_banner.widget.dart
+++ lib/widgets/mutable_banner/mutable_banner.widget.dart
@@ -2,7 +2,6 @@ import 'package:figma_squircle/figma_squircle.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:safe/core.dart';
-import 'package:safe/screens/welcome/welcome.screen.dart';
 import 'package:safe/utils/constants/constants.util.dart';
 import 'package:safe/widgets/mutable_button/mutable_button.widget.dart';
 import 'package:safe/widgets/mutable_text/mutable_text.widget.dart';
@@ -15,11 +14,15 @@ enum MessageType {
 class MutableBanner extends StatefulWidget {
   final MessageType type;
   final String title;
+  final BannerController? controller;
+  final Duration? duration;
   final String description;
   final void Function()? onTap;
 
   MutableBanner({
     this.type = MessageType.success,
+    this.controller,
+    this.duration,
     this.title = "",
     this.description = "",
     this.onTap,
@@ -29,88 +32,165 @@ class MutableBanner extends StatefulWidget {
   State<MutableBanner> createState() => _MutableBannerState();
 }
 
-class _MutableBannerState extends State<MutableBanner> {
+class _MutableBannerState extends State<MutableBanner>
+    with TickerProviderStateMixin {
   late Core core;
+  late MediaQueryData queryData;
+
+  // Properties for animation
+  late AnimationController controller;
+  late Animation animation;
+  double topPosition = -120;
+  bool dismissed = false;
 
   @override
   void initState() {
     super.initState();
 
     core = Provider.of<Core>(context, listen: false);
+
+    if (widget.controller != null) {
+      widget.controller!.setState(this);
+    }
+
+    // Initialize controller
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 200),
+    );
+
+    // Initialize animation
+    animation = Tween<double>(begin: -120.0, end: 0.0).animate(
+      CurvedAnimation(parent: controller, curve: Curves.decelerate),
+    );
+
+    controller.addListener(() {
+      setState(() {
+        topPosition = animation.value;
+      });
+    });
+  }
+
+  Future<void> show() async {
+    dismissed = false;
+    await controller.forward();
+    await Future.delayed(widget.duration ?? Duration(seconds: 3));
+    if (!dismissed) {
+      await controller.reverse();
+    }
+  }
+
+  Future<void> dismiss() async {
+    dismissed = true;
+    await controller.reverse();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 115,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            kColorMap[MutableColor.neutral10]!
-                .withOpacity(kTransparencyMap[Transparency.v80]!),
-            kColorMap[MutableColor.neutral10]!.withOpacity(0),
-          ],
+    queryData = MediaQuery.of(context);
+    return Positioned(
+      top: topPosition,
+      child: Container(
+        width: queryData.size.width,
+        height: 115,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              kColorMap[MutableColor.neutral10]!
+                  .withOpacity(kTransparencyMap[Transparency.v80]!),
+              kColorMap[MutableColor.neutral10]!.withOpacity(0),
+            ],
+          ),
         ),
-      ),
-      padding: EdgeInsets.symmetric(
-        horizontal: kSideScreenMargin,
-      ),
-      child: Align(
-        alignment: Alignment.bottomCenter,
-        child: MutableButton(
-          onTap: widget.onTap,
-          child: Container(
-            width: double.infinity,
-            height: 64,
-            padding: EdgeInsets.symmetric(horizontal: 15),
-            decoration: ShapeDecoration(
-              color: core.utils.color.translucify(
-                widget.type == MessageType.success
-                    ? MutableColor.secondaryGreen
-                    : MutableColor.secondaryRed,
-                Transparency.v16,
-              ),
-              shape: SmoothRectangleBorder(
-                borderRadius: SmoothBorderRadius(
-                  cornerSmoothing: kCornerSmoothing,
-                  cornerRadius: 16,
-                ),
-                side: BorderSide(
-                  color: kColorMap[widget.type == MessageType.success
+        padding: EdgeInsets.symmetric(
+          horizontal: kSideScreenMargin,
+        ),
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: MutableButton(
+            onTap: () {
+              dismiss();
+              if (widget.onTap != null) {
+                widget.onTap!();
+              }
+            },
+            child: Container(
+              width: double.infinity,
+              height: 64,
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              decoration: ShapeDecoration(
+                color: core.utils.color.translucify(
+                  widget.type == MessageType.success
                       ? MutableColor.secondaryGreen
-                      : MutableColor.secondaryRed]!,
-                  width: kBorderWidth,
+                      : MutableColor.secondaryRed,
+                  Transparency.v16,
+                ),
+                shape: SmoothRectangleBorder(
+                  borderRadius: SmoothBorderRadius(
+                    cornerSmoothing: kCornerSmoothing,
+                    cornerRadius: 16,
+                  ),
+                  side: BorderSide(
+                    color: kColorMap[widget.type == MessageType.success
+                        ? MutableColor.secondaryGreen
+                        : MutableColor.secondaryRed]!,
+                    width: kBorderWidth,
+                  ),
                 ),
               ),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                MutableText(
-                  widget.title,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  weight: TypeWeight.semiBold,
-                  style: TypeStyle.body,
-                ),
-                SizedBox(height: 2),
-                MutableText(
-                  widget.description,
-                  maxLines: 1,
-                  size: 13,
-                  overflow: TextOverflow.ellipsis,
-                  weight: TypeWeight.regular,
-                  color: MutableColor.neutral2,
-                ),
-              ],
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  MutableText(
+                    widget.title,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    weight: TypeWeight.semiBold,
+                    style: TypeStyle.body,
+                  ),
+                  SizedBox(height: 2),
+                  MutableText(
+                    widget.description,
+                    maxLines: 1,
+                    size: 13,
+                    overflow: TextOverflow.ellipsis,
+                    weight: TypeWeight.regular,
+                    color: MutableColor.neutral2,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
+  }
+}
+
+class BannerController {
+  _MutableBannerState? _state;
+
+  // ignore: library_private_types_in_public_api
+  void setState(_MutableBannerState state) => _state = state;
+
+  bool get isAttached => _state != null;
+
+  Future<void> show() async {
+    assert(_state != null, "Controller has not been attached");
+    await _state!.show();
+  }
+
+  Future<void> dismiss() async {
+    assert(_state != null, "Controller has not been attached");
+    await _state!.dismiss();
   }
 }
