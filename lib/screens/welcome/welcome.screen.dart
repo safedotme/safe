c98848cbb1e7a3ea6c0d42d256707adf88db8570
Mark Music <markmusic999@gmail.com>
2022-07-04 21:05:38 -0600

    ft[welcome] => 🧦 Build rudimentary bubble animation

--- lib/screens/welcome/welcome.screen.dart
+++ lib/screens/welcome/welcome.screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 import 'package:safe/core.dart';
 import 'package:safe/widgets/mutable_banner/mutable_banner.widget.dart';
@@ -27,26 +28,110 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return MutableScaffold(
-        overlays: [
-          MutableBanner(
-            title: "This is the title",
-            description: "This is the description",
-            controller: controller,
+      overlays: [
+        // List.generate
+        Bubble(0),
+      ],
+      body: Container(),
+    );
+  }
+}
+
+class Bubble extends StatefulWidget {
+  final int index;
+
+  Bubble(this.index);
+  @override
+  State<Bubble> createState() => _BubbleState();
+}
+
+class _BubbleState extends State<Bubble> with TickerProviderStateMixin {
+  late double size;
+  late AnimationController controller;
+  late Animation animation;
+  double scale = 1;
+
+  Future<void> animate() async {
+    int baseline = 1080;
+    int increment = 352;
+    // Animate into scene
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: baseline + (increment * widget.index)),
+    );
+
+    animation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: controller, curve: Curves.ease),
+    );
+
+    controller.addListener(() {
+      setState(() {
+        scale = animation.value;
+      });
+    });
+
+    await controller.forward();
+
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1500),
+    );
+
+    animation = Tween<double>(begin: 1, end: 1.15).animate(
+      CurvedAnimation(parent: controller, curve: Curves.easeInOut),
+    );
+
+    controller.addListener(() {
+      setState(() {
+        scale = animation.value;
+      });
+    });
+
+    controller.repeat(reverse: true);
+  }
+
+  // Generates size
+  void genSize() {
+    double baseline = 540;
+    double increment = 176;
+
+    size = baseline + (increment * widget.index);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Generates base size for bubbles
+    genSize();
+
+    // Animate bubble
+    animate();
+  }
+
+  // 540
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 50,
+      right: -80,
+      child: Transform.scale(
+        scale: scale,
+        child: Container(
+          height: size,
+          width: size,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.red[(widget.index + 1) * 100],
           ),
-        ],
-        body: SizedBox.expand(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  controller.show();
-                },
-                child: MutableText("Show Banner"),
-              ),
-            ],
-          ),
-        ));
+        ),
+      ),
+    );
   }
 }
