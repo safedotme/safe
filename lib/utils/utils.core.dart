0a18dd676a6891e4993e8ae750e7196771f4849a
Mark Music <markmusic999@gmail.com>
2022-07-04 18:06:28 -0600

    chr => 🧶 Create develop branch

--- lib/utils/utils.core.dart
+++ lib/utils/utils.core.dart
@@ -5,7 +5,7 @@ class Utils {
   ColorUtils color = ColorUtils();
   Logger log = Logger(
     printer: PrettyPrinter(
-        lineLength: 120, // width of the output
+        lineLength: 80, // Width of the output
         colors: true, // Colorful log messages
         printEmojis: true, // Print an emoji for each log message
         printTime: false // Should each log print contain a timestamp
