ecdb209233dae9c895bbbe525bf69d88fd0cf60f
Mark Music <markmusic999@gmail.com>
2022-07-08 23:53:19 -0600

    ft[icons] => ☎️ Add phone icon as CustomPainter and into icon util

--- lib/utils/icon/icon.util.dart
+++ lib/utils/icon/icon.util.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:safe/widgets/mutable_icon/local_widgets/checkmark.icon.dart';
 import 'package:safe/widgets/mutable_icon/local_widgets/key.icon.dart';
+import 'package:safe/widgets/mutable_icon/local_widgets/phone.icon.dart';
 import 'package:safe/widgets/mutable_icon/local_widgets/profile.icon.dart';
 
 enum MutableIcons {
+  phone,
   profile,
   checkmark,
   key,
@@ -17,6 +19,7 @@ class IconUtil {
       MutableIcons.profile: (c) => ProfileIcon(c),
       MutableIcons.checkmark: (c) => CheckmarkIcon(c),
       MutableIcons.key: (c) => KeyIcon(c),
+      MutableIcons.phone: (c) => PhoneIcon(c),
     };
   }
 }
