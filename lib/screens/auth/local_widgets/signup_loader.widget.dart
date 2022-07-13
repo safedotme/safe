c0a87ac261f3948fcb13314e6875442e6c283303
Mark Music <markmusic999@gmail.com>
2022-07-13 15:14:22 -0600

    ft[auth/signup_loader] => 🐋 Implement loader to signup flow

--- lib/screens/auth/local_widgets/signup_loader.widget.dart
+++ lib/screens/auth/local_widgets/signup_loader.widget.dart
@@ -3,9 +3,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:safe/core.dart';
-import 'package:safe/utils/constants/constants.util.dart';
+import 'package:safe/widgets/mutable_loader/mutable_loader.widget.dart';
 import 'package:safe/widgets/mutable_overlay/mutable_overlay.widget.dart';
-import 'package:safe/widgets/mutable_text/mutable_text.widget.dart';
 
 class SignupLoader extends StatefulWidget {
   @override
@@ -25,45 +24,11 @@ class _SignupLoaderState extends State<SignupLoader> {
   @override
   Widget build(BuildContext context) {
     return MutableOverlay(
-      controller: core.state.signup.overlayController..show(),
+      controller: core.state.signup.overlayController,
       child: Center(
         child: MutableLoader(
           text: "Creating account",
         ),
-      ),
-    );
-  }
-}
-
-class MutableLoader extends StatelessWidget {
-  final String text;
-
-  MutableLoader({required this.text});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(24),
-      decoration: ShapeDecoration(
-        color: kColorMap[MutableColor.iosGrey],
-        shape: SmoothRectangleBorder(
-          borderRadius: SmoothBorderRadius(
-            cornerRadius: 20,
-            cornerSmoothing: kCornerSmoothing,
-          ),
-        ),
-      ),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          CupertinoActivityIndicator(color: Colors.white),
-          SizedBox(width: 10),
-          MutableText(
-            "$text...",
-            style: TypeStyle.h4,
-            weight: TypeWeight.semiBold,
-          ),
-        ],
       ),
     );
   }
