5abad01cb3d00f41524bef1a29b65ef6c07c5ef6
Mark Music <markmusic999@gmail.com>
2022-07-14 22:02:01 -0600

    ft[widgets/mutable_auth_wrapper] => 🌻 Add AuthWrapper with stream to detect when a user is signed in

--- lib/screens/home/home.screen.dart
+++ lib/screens/home/home.screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:provider/provider.dart';
 import 'package:safe/core.dart';
 import 'package:safe/widgets/mutable_scaffold/mutable_scaffold.widget.dart';
@@ -14,12 +12,16 @@ class HomeScreen extends StatelessWidget {
     Core core = Provider.of<Core>(context, listen: false);
     return MutableScaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            core.utils.auth.signOut();
-          },
-          child: Center(
-            child: MutableText("Sign Out"),
+        child: SizedBox(
+          height: 100,
+          width: 100,
+          child: ElevatedButton(
+            onPressed: () {
+              core.utils.auth.signOut();
+            },
+            child: Center(
+              child: MutableText("Sign Out"),
+            ),
           ),
         ),
       ),
