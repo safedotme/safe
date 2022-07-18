c55ca16c063a20cdfd2c843406e71988accf1e73
Mark Music <markmusic999@gmail.com>
2022-07-18 17:44:19 -0600

    ft[server/incidents] => 🥻 Finish incident CRUD foundation

--- lib/services/server/incident_server.service.dart
+++ lib/services/server/incident_server.service.dart
@@ -1,12 +1,35 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:safe/models/incident/incident.model.dart';
 
 class IncidentServer {
   final FirebaseFirestore _db;
+  static String path = "incidents";
 
   IncidentServer(this._db);
   // INCIDENTS
 
   // -> READ
+  Stream<List<Incident>> readFromUserId({required String userId}) {
+    return _db
+        .collection(path)
+        .where("user_id", isEqualTo: userId)
+        .snapshots()
+        .map(
+          (snapshot) => snapshot.docs
+              .map(
+                (doc) => Incident.fromJson(doc.data()),
+              )
+              .toList(),
+        );
+  }
+
   // -> UPSERT
+  Future<void> upsert(Incident incident) {
+    var options = SetOptions(merge: true);
+
+    return _db.collection(path).doc(incident.id).set(incident.toMap(), options);
+  }
+
   // -> DELETE
+  Future<void> delete(String id) => _db.collection(path).doc(id).delete();
 }
