31e7ae06563cc70c791d25a0f4bccaa9bd0b0c6c
Mark Music <markmusic999@gmail.com>
2022-07-17 22:06:33 -0600

    ft[icons] => 🤴 Add custom painter widgets for menu and play large icon used in incident card widget

---
+++ lib/widgets/mutable_icon/local_widgets/menu.icon.dart
@@ -0,0 +1,124 @@
+//Copy this CustomPainter code to the Bottom of the File
+import 'package:flutter/material.dart';
+
+class MenuIcon extends CustomPainter {
+  final Color color;
+
+  MenuIcon(this.color);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Path path_0 = Path();
+    path_0.moveTo(size.width * 0.6428571, size.height * 0.1785714);
+    path_0.cubicTo(
+        size.width * 0.6428571,
+        size.height * 0.2180204,
+        size.width * 0.5788979,
+        size.height * 0.2500000,
+        size.width * 0.5000000,
+        size.height * 0.2500000);
+    path_0.cubicTo(
+        size.width * 0.4211021,
+        size.height * 0.2500000,
+        size.width * 0.3571429,
+        size.height * 0.2180204,
+        size.width * 0.3571429,
+        size.height * 0.1785714);
+    path_0.cubicTo(
+        size.width * 0.3571429,
+        size.height * 0.1391225,
+        size.width * 0.4211021,
+        size.height * 0.1071429,
+        size.width * 0.5000000,
+        size.height * 0.1071429);
+    path_0.cubicTo(
+        size.width * 0.5788979,
+        size.height * 0.1071429,
+        size.width * 0.6428571,
+        size.height * 0.1391225,
+        size.width * 0.6428571,
+        size.height * 0.1785714);
+    path_0.close();
+
+    Paint paint_0_fill = Paint()..style = PaintingStyle.fill;
+    paint_0_fill.color = Color(0xffFBFCFF).withOpacity(1.0);
+    canvas.drawPath(path_0, paint_0_fill);
+
+    Path path_1 = Path();
+    path_1.moveTo(size.width * 0.6428571, size.height * 0.4285714);
+    path_1.cubicTo(
+        size.width * 0.6428571,
+        size.height * 0.4680214,
+        size.width * 0.5788979,
+        size.height * 0.5000000,
+        size.width * 0.5000000,
+        size.height * 0.5000000);
+    path_1.cubicTo(
+        size.width * 0.4211021,
+        size.height * 0.5000000,
+        size.width * 0.3571429,
+        size.height * 0.4680214,
+        size.width * 0.3571429,
+        size.height * 0.4285714);
+    path_1.cubicTo(
+        size.width * 0.3571429,
+        size.height * 0.3891214,
+        size.width * 0.4211021,
+        size.height * 0.3571429,
+        size.width * 0.5000000,
+        size.height * 0.3571429);
+    path_1.cubicTo(
+        size.width * 0.5788979,
+        size.height * 0.3571429,
+        size.width * 0.6428571,
+        size.height * 0.3891214,
+        size.width * 0.6428571,
+        size.height * 0.4285714);
+    path_1.close();
+
+    Paint paint_1_fill = Paint()..style = PaintingStyle.fill;
+    paint_1_fill.color = Color(0xffFBFCFF).withOpacity(1.0);
+    canvas.drawPath(path_1, paint_1_fill);
+
+    Path path_2 = Path();
+    path_2.moveTo(size.width * 0.6428571, size.height * 0.6785714);
+    path_2.cubicTo(
+        size.width * 0.6428571,
+        size.height * 0.7180214,
+        size.width * 0.5788979,
+        size.height * 0.7500000,
+        size.width * 0.5000000,
+        size.height * 0.7500000);
+    path_2.cubicTo(
+        size.width * 0.4211021,
+        size.height * 0.7500000,
+        size.width * 0.3571429,
+        size.height * 0.7180214,
+        size.width * 0.3571429,
+        size.height * 0.6785714);
+    path_2.cubicTo(
+        size.width * 0.3571429,
+        size.height * 0.6391214,
+        size.width * 0.4211021,
+        size.height * 0.6071429,
+        size.width * 0.5000000,
+        size.height * 0.6071429);
+    path_2.cubicTo(
+        size.width * 0.5788979,
+        size.height * 0.6071429,
+        size.width * 0.6428571,
+        size.height * 0.6391214,
+        size.width * 0.6428571,
+        size.height * 0.6785714);
+    path_2.close();
+
+    Paint paint_2_fill = Paint()..style = PaintingStyle.fill;
+    paint_2_fill.color = color;
+    canvas.drawPath(path_2, paint_2_fill);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
--- lib/widgets/mutable_incident_card/local_widgets/incident_card_image.widget.dart
+++ lib/widgets/mutable_incident_card/local_widgets/incident_card_image.widget.dart
@@ -1,10 +1,33 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class IncidentCardImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
+    return Stack(
+      children: [
+        SizedBox.expand(
+          child: CachedNetworkImage(
+            imageUrl:
+                "https://d279m997dpfwgl.cloudfront.net/wp/2020/06/GettyImages-1221138690.jpg",
+            fit: BoxFit.cover,
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.black.withOpacity(0.3),
+                Colors.transparent,
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+        ),
+        // Play Button
+        // Menu
+      ],
     );
   }
 }
