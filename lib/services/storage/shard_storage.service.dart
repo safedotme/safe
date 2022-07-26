2979971eb4aa878937e7088f1c437f616ca08481
Mark Music <markmusic999@gmail.com>
2022-07-26 13:37:19 -0600

    ft[services/storage] => 🐜 Build algorithm to select bucket based on previous shard distribution

--- lib/services/storage/shard_storage.service.dart
+++ lib/services/storage/shard_storage.service.dart
@@ -17,11 +17,75 @@ class ShardStorageService {
 
   String? get bucket => _instance?.bucket;
 
-  /// Will set a random bucket to [_instance]
-  void setBucket() {
-    var chosen = Random().nextInt(_buckets.length);
+  /// Used to set an explicit bucket
+  void setBucket(String id) =>
+      _instance = FirebaseStorage.instanceFor(bucket: id);
 
-    _instance = FirebaseStorage.instanceFor(bucket: _buckets[chosen]);
+  /// Chooses a bucket based on previous bucket distribution
+  void setDistributedBucket(List<String> usedBuckets) {
+    late bool isDistributed;
+
+    // Generates distribution
+    var distribution = _generateDistribution(
+      usedBuckets,
+      _buckets,
+    );
+
+    // Checks for even distribution
+    isDistributed = _checkDistribution(distribution);
+
+    if (isDistributed) {
+      var rnd = Random().nextInt(_buckets.length);
+      setBucket(_buckets[rnd]);
+
+      return;
+    }
+
+    int? smallest;
+
+    for (String key in distribution.keys) {
+      smallest ??= distribution[key]!;
+
+      if (smallest > distribution[key]!) {
+        smallest = distribution[key]!;
+      }
+    }
+
+    // Select smallest key
+    var bucket = distribution.keys.firstWhere(
+      (k) => distribution[k] == smallest,
+    );
+
+    setBucket(bucket);
+  }
+
+  bool _checkDistribution(Map<String, int> chart) {
+    int sum = 0;
+    int? product;
+
+    for (String key in chart.keys) {
+      product ??= chart[key]! * chart.length;
+      sum = chart[key]! + sum;
+    }
+
+    return (sum / product!) == 1;
+  }
+
+  Map<String, int> _generateDistribution(
+    List<String> base,
+    List<String> checkAgainst,
+  ) {
+    Map<String, int> distributionChart = {};
+
+    for (String c in checkAgainst) {
+      distributionChart[c] = 0;
+    }
+
+    for (String b in base) {
+      distributionChart[b] = distributionChart[b]! + 1;
+    }
+
+    return distributionChart;
   }
 
   void fetch(String path) {}
