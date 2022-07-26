04506b5ae979c68edcc8ff571777d2be0ee37aac
Mark Music <markmusic999@gmail.com>
2022-07-26 12:19:07 -0600

    ft[storage] => 🦧 Add links to sharded buckets

--- lib/services/storage/storage.service.dart
+++ lib/services/storage/storage.service.dart
@@ -1,5 +1,11 @@
 import 'package:firebase_storage/firebase_storage.dart';
 
 class StorageService {
-  final FirebaseStorage _instance = FirebaseStorage.instance;
+  final List<String> _buckets = [
+    "gs://safe-f73bf-2iwra",
+    "gs://safe-f73bf-2iymj",
+    "gs://safe-f73bf-6rjlm",
+    "gs://safe-f73bf-he25n",
+    "gs://safe-f73bf-yomzj",
+  ];
 }
