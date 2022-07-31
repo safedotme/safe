2335a38a7999cf0652649f5aae86eb939dbb8f81
Mark Music <markmusic999@gmail.com>
2022-07-31 10:32:07 -0600

    ft[services/signaling] => 🌷 Build signaling frame (empty methods)

--- lib/services/signaling/signaling.service.dart
+++ lib/services/signaling/signaling.service.dart
@@ -1,6 +1,17 @@
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+
+typedef StreamStateCallback = void Function(MediaStream stream);
+
 class SignalingService {
+  RTCPeerConnection? peerConnection;
+  MediaStream? localStream;
+  MediaStream? remoteStream;
+  String? roomId;
+  String? currentRoomText;
+  StreamStateCallback? onAddRemoteStream;
+
   // Configuration for testing (will change later)
-  Map<String, dynamic> configuration = {
+  static const Map<String, dynamic> config = {
     'iceServers': [
       {
         'urls': [
@@ -10,4 +21,13 @@ class SignalingService {
       }
     ]
   };
+
+  /// Used by genesis client to create a session (call)
+  Future<String> createSession(RTCVideoRenderer remoteRenderer) async {}
+
+  /// Used by external clients to join a session
+  Future<void> joinSession() async {}
+
+  /// Gets media information for all clients
+  Future<void> openLocalMedia() async {}
 }
