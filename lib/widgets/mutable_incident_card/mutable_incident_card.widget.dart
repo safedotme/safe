72a53a09cd41f5fc5d045b62e3386b49a60605dd
Mark Music <markmusic999@gmail.com>
2023-01-02 01:24:51 -0600

    ft[incident_log] => 🫡 Implement incident screen on card tap

--- lib/widgets/mutable_incident_card/mutable_incident_card.widget.dart
+++ lib/widgets/mutable_incident_card/mutable_incident_card.widget.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:safe/core.dart';
 import 'package:safe/models/incident/incident.model.dart';
 import 'package:safe/utils/constants/constants.util.dart';
 import 'package:safe/widgets/mutable_button/mutable_button.widget.dart';
 import 'package:safe/widgets/mutable_incident_card/local_widgets/incident_card_body.widget.dart';
 import 'package:safe/widgets/mutable_incident_card/local_widgets/incident_card_image.widget.dart';
 
-class MutableIncidentCard extends StatelessWidget {
+class MutableIncidentCard extends StatefulWidget {
   final Incident incident;
 
   MutableIncidentCard(this.incident);
 
   @override
+  State<MutableIncidentCard> createState() => _MutableIncidentCardState();
+}
+
+class _MutableIncidentCardState extends State<MutableIncidentCard> {
+  late Core core;
+
+  @override
+  void initState() {
+    super.initState();
+    core = Provider.of<Core>(context, listen: false);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MutableButton(
+      onTap: () {
+        core.state.incident.setIncident(widget.incident);
+        core.state.incident.controller.open();
+      },
       child: Container(
         decoration: BoxDecoration(
           color: kColorMap[kIncidentCardBgColor],
@@ -28,7 +47,7 @@ class MutableIncidentCard extends StatelessWidget {
             mainAxisSize: MainAxisSize.min,
             children: [
               IncidentCardImage(
-                incident,
+                widget.incident,
                 onPlayTap: () {
                   print("play");
                 },
@@ -36,7 +55,7 @@ class MutableIncidentCard extends StatelessWidget {
                   print("menu");
                 },
               ),
-              IncidentCardBody(incident),
+              IncidentCardBody(widget.incident),
             ],
           ),
         ),
