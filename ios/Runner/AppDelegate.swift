6c7776fd86f2f9126258f2f27e564b7dde453ffd
Mark Music <markmusic999@gmail.com>
2023-01-03 21:52:58 -0600

    ft => 🎉 Remove AppDelegate from git

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,7 +10,7 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     FirebaseApp.configure()
-    GMSServices.provideAPIKey("test")
+    GMSServices.provideAPIKey("yoo")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
