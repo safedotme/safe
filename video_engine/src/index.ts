f1603740dabc431f076963853cb872c8e28a9232
Mark Music <markmusic999@gmail.com>
2023-01-05 13:39:51 -0600

    ft => 🌵 Setup typescript project

--- video_engine/src/index.ts
+++ video_engine/src/index.ts
@@ -1,1 +1,6 @@
-import fs from "fs";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "./firebaseConfig.js"
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
