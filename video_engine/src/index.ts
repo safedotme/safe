3aa65259d181d111ed45ba904ce4c44d60c28d6b
Mark Music <markmusic999@gmail.com>
2023-01-06 16:37:14 -0600

    chr => 🎄 Remove video engine

--- video_engine/src/index.ts
+++
@@ -1,6 +0,0 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { firebaseConfig } from "./firebaseConfig.js"
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
