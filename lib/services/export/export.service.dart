42638e1d7ae5032823c2a0c5a76a4687116231ac
Mark Music <markmusic999@gmail.com>
2023-01-17 22:03:46 -0600

    ft[services/export] => 🎭 Finish location model translation to csv

--- lib/services/export/export.service.dart
+++ lib/services/export/export.service.dart
@@ -31,13 +31,18 @@ class ExportService {
     }
 
     // ⬇️ DOWNLOAD WATERMARKED VIDEO
-    // final video = await _downloadVideo(i.path, directory);
+    final video = await _downloadVideo(i.path, directory);
 
     // ⬇️ PROCESS BATTERY DATA
-    //final battery = await _processBatteryLog(i.battery, directory);
+    final battery = await _processBatteryLog(i.battery, directory);
 
-    // ⬇️ PROCESS CONTACT
-    //final battery = await _processBatteryLog(i.battery, directory);
+    // ⬇️ PROCESS CONTACT DATA
+    final contact = await _processContactLog(i.contactLog, directory);
+
+    // ⬇️ PROCESS LOCATION DATA
+    final location = await _processLocationLog(i.location, directory);
+
+    // TODO: Delete directory after
   }
 
   Future<File?> _processBatteryLog(List<Battery>? b, Directory dir) async {
@@ -74,12 +79,106 @@ class ExportService {
     return f.writeAsString(csv);
   }
 
-  Future<File?> _processContactLog(List<NotifiedContact>? b) async {
-    return null;
+  Future<File?> _processContactLog(
+      List<NotifiedContact>? c, Directory dir) async {
+    if (c == null || c.isEmpty) return null;
+
+    List<List<dynamic>> rows = [];
+
+    // ADD ROWS
+    rows.add(["Date", "Time", "Contact Name", "Phone Number", "Message Type"]);
+
+    // POPULATE ROWS
+    for (final contact in c) {
+      List<dynamic> row = [];
+      // DATE
+      row.add(
+        "${DateFormat.yMMMMd().format(contact.datetime)} (${contact.datetime.timeZoneName})",
+      );
+
+      // TIME
+      row.add(
+        "${DateFormat.jms().format(contact.datetime)} (${contact.datetime.timeZoneName})",
+      );
+
+      // CONTACT NAME
+      row.add(contact.name);
+
+      // PHONE NUMBER
+      row.add(contact.phone);
+
+      // MESSAGE TYPE
+      String type = contact.type.toString();
+      row.add(
+        type.substring(type.indexOf(".") + 1, type.length),
+      );
+
+      rows.add(row);
+    }
+
+    String csv = const ListToCsvConverter().convert(rows);
+
+    File f = File("${dir.path}/contact_log.csv");
+
+    return f.writeAsString(csv);
   }
 
-  Future<File?> _processLocationLog(List<Location>? b) async {
-    return null;
+  Future<File?> _processLocationLog(List<Location>? l, Directory dir) async {
+    if (l == null || l.isEmpty) return null;
+
+    List<List<dynamic>> rows = [];
+
+    // ADD ROWS
+    rows.add([
+      "Date",
+      "Time",
+      "Latitude",
+      "Longitude",
+      "Device Accuracy (m)",
+      "Speed (m/s)",
+      "Altitude (m)",
+      "Address",
+    ]);
+
+    // POPULATE ROWS
+    for (final location in l) {
+      List<dynamic> row = [];
+      // DATE
+      row.add(
+        "${DateFormat.yMMMMd().format(location.datetime)} (${location.datetime.timeZoneName})",
+      );
+
+      // TIME
+      row.add(
+        "${DateFormat.jms().format(location.datetime)} (${location.datetime.timeZoneName})",
+      );
+
+      // LATITUDE
+      row.add(location.lat);
+
+      // LONGITUDE
+      row.add(location.long);
+
+      // DEVICE ACCURACY
+      row.add(location.accuracy);
+
+      // SPEED
+      row.add(location.speed);
+
+      // ALTITUDE
+      row.add(location.alt);
+
+      // ADDRESS
+      row.add(location.address);
+
+      rows.add(row);
+    }
+
+    String csv = const ListToCsvConverter().convert(rows);
+
+    File f = File("${dir.path}/location_log.csv");
+
+    return f.writeAsString(csv);
   }
 
   Future<File?> _downloadVideo(String? path, Directory dir) async {
