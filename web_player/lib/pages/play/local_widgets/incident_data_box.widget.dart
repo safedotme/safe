3bcceefa4d1a30b8ca33f73c485b3b9e34d520d6
Mark Music <markmusic999@gmail.com>
2023-01-29 21:48:03 -0600

    ft[ticker] => 👏 Add ticker to update datetime timestamp

--- web_player/lib/pages/play/local_widgets/incident_data_box.widget.dart
+++ web_player/lib/pages/play/local_widgets/incident_data_box.widget.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:safe/core.dart';
+import 'package:safe/models/incident/incident.model.dart';
 import 'package:safe/pages/play/local_widgets/live_pill.widget.dart';
 import 'package:safe/utils/constants/constants.util.dart';
 import 'package:safe/widgets/mutable_text/mutable_text.widget.dart';
@@ -12,11 +15,42 @@ class IncidentDataBox extends StatefulWidget {
 
 class _IncidentDataBoxState extends State<IncidentDataBox> {
   late Core core;
+  String timestamp = "";
+  late Timer timer;
 
   @override
   void initState() {
     super.initState();
     core = Provider.of<Core>(context, listen: false);
+    ticker();
+  }
+
+  void ticker() {
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        timestamp = generateDateTimeStamp(core.state.play.incident!);
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    if (timer.isActive) {
+      timer.cancel();
+    }
+    super.dispose();
+  }
+
+  String generateDateTimeStamp(Incident i) {
+    final start = i.datetime;
+
+    final timezone = start.timeZoneName.replaceAll(RegExp(r"[a-z ]"), "");
+    final diff = DateTime.now().difference(start);
+    final mins = diff.inMinutes;
+    final secs = (diff.inSeconds - diff.inMinutes * 60);
+    final date = DateFormat.jm().format(start.add(diff));
+
+    return "${mins < 10 ? "0$mins" : mins}:${secs < 10 ? "0$secs" : secs} - $date ($timezone)";
   }
 
   @override
@@ -40,7 +74,7 @@ class _IncidentDataBoxState extends State<IncidentDataBox> {
               ),
               SizedBox(height: 3),
               MutableText(
-                "9:41 - 12:30 PM (EST)",
+                timestamp,
                 size: 14,
                 color: MutableColor.neutral2,
               )
